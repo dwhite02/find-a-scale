44956d46cbb45df60a9c6caea6143e5c2abb995a
Donte White <dwhite02@syr.edu>
2024-11-27 18:06:47 -0800

    add additional styling for initial screen

--- ClientApp/src/components/InitialScreen/InitialScreen.js
+++ ClientApp/src/components/InitialScreen/InitialScreen.js
@@ -4,14 +4,15 @@ import './InitialScreen.scss'
 const InitialScreen = ({onClick}) => {
     return (
         <>
-            <div className='vh-100 d-flex justify-content-center align-items-center px-4'>
+            <div className='vh-100 d-flex justify-content-center align-items-center px-4 welcome-screen'>
                 <div className='d-flex callout-hero'>
                     <div className='callout-hero__content'>
                         <h1 className='headline headline--splash'> Find A  Scale </h1>
                         <button className='btn btn-dark ' onClick={onClick}> Go Find Your Scale</button>
                     </div>
                     <div className='callout-hero__image'>
-                        <span class="loader"></span>
+                        <span className="loader"></span>
+                        <span className='shadow'></span>
                     </div>
                     
                 </div>
