52756994ff08d28f6e9fc2f5f19d9cc1fbf34934
Donte White <dwhite02@syr.edu>
2024-11-27 17:07:21 -0800

    create sass default folder, update initial screen styling

--- ClientApp/src/components/InitialScreen/InitialScreen.js
+++ ClientApp/src/components/InitialScreen/InitialScreen.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import './InitialScreen.scss'
 
 const InitialScreen = ({onClick}) => {
     return (
         <>
-            <div>
-                <h1> Find A Scale </h1>
-                <button onClick={onClick}> Go Find Your Scale</button>
+            <div className='vh-100 d-flex justify-content-center align-items-center px-4'>
+                <div className='d-flex callout-hero'>
+                    <div className='callout-hero__content'>
+                        <h1 className='headline headline--splash'> Find A  Scale </h1>
+                        <button className='btn btn-dark ' onClick={onClick}> Go Find Your Scale</button>
+                    </div>
+                    <div className='callout-hero__image'>
+                        <span class="loader"></span>
+                    </div>
+                    
+                </div>
+                
             </div>
         </>
     )
